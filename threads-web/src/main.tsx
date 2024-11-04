31eb54430d38d91024469fda10064bb161086b95
Ha Yen <hanguyentieuyen@gmail.com>
2024-11-04 23:51:25 +0700

    feat: errorboundary

--- threads-web/src/main.tsx
+++ threads-web/src/main.tsx
@@ -4,7 +4,7 @@ import App from './App.tsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-
+import ErrorBoundary from './components/ErrorBoundary'
 // Create query client
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -19,7 +19,9 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
-        <App />
+        <ErrorBoundary>
+          <App />
+        </ErrorBoundary>
       </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>
