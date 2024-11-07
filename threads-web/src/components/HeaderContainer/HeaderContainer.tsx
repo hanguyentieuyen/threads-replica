87344709e96e03c9f4a73079cad6c92097320564
Ha Yen <hanguyentieuyen@gmail.com>
2024-11-08 00:53:00 +0700

    feat: update HeaderContainer

--- threads-web/src/components/HeaderContainer/HeaderContainer.tsx
+++ threads-web/src/components/HeaderContainer/HeaderContainer.tsx
@@ -10,17 +10,29 @@ import {
   DropdownMenuTrigger
 } from "../Dropdown/Dropdown"
 import { ChevronDown } from "lucide-react"
-
+import Button from "../Button"
+const dropdownItems = ["Dành cho bạn", "option 2", "option 3", "option 4", "option 5"]
 export default function HeaderContainer() {
   const [isChecked, setIsChecked] = useState(false)
+  const [labelBtn, setLabelBtn] = useState<string>(dropdownItems[0])
+  const handleSelectItem = (item: string) => {
+    setLabelBtn(item)
+  }
   return (
     <div className='max-w-2xl mx-auto flex items-center justify-center h-[60px] bg-slate-50'>
+      <Button className='min-w-20 font-semibold text-gray-700 text-sm'>{labelBtn}</Button>
       <DropdownMenu>
         <DropdownMenuTrigger>
-          <ChevronDown size={18} />
+          <div className='w-6 h-6 bg-white rounded-full flex justify-center items-center border shadow-sm'>
+            <ChevronDown size={18} />
+          </div>
         </DropdownMenuTrigger>
         <DropdownMenuContent>
-          <DropdownMenuItem>option 1</DropdownMenuItem>
+          {dropdownItems.map((dropdownItem, index) => (
+            <DropdownMenuItem key={index} onClick={() => handleSelectItem(dropdownItem)}>
+              {dropdownItem}
+            </DropdownMenuItem>
+          ))}
           <DropdownMenuCheckboxItem checked={isChecked} onCheckedChange={setIsChecked}>
             Checkbox Item
           </DropdownMenuCheckboxItem>
